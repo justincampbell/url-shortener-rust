fe5af3541e1b985ec79754b12de814323f547241
Justin Campbell <justin@justincampbell.me>
2015-01-07 11:48:06 -0500

    Prefer borrowing over cloning

--- src/shortener.rs
+++ src/shortener.rs
@@ -13,11 +13,11 @@ impl World {
         World { urls: HashMap::new(), id: 0i }
     }
 
-    pub fn shorten(&mut self, url: Url) -> Token {
+    pub fn shorten(&mut self, url: &Url) -> Token {
         self.id += 1;
         let token = World::tokenize(self.id);
 
-        self.urls.insert(token.to_string(), url);
+        self.urls.insert(token.to_string(), url.clone());
 
         token
     }
@@ -38,8 +38,8 @@ fn shorten_and_expand() {
     let url1 = "justincampbell.me".to_string();
     let url2 = "justincampbell.me".to_string();
 
-    let token1 = world.shorten(url1.clone());
-    let token2 = world.shorten(url2.clone());
+    let token1 = world.shorten(&url1);
+    let token2 = world.shorten(&url2);
 
     match world.expand(token1) {
         Some(expanded_url) => assert_eq!(&url1, expanded_url),
